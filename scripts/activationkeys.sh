24ec74645a03f9ba8f85cea87618ded47124720c
root <martin.weiss@suse.com>
2021-04-01 15:38:36 +0200

    further optimizations

--- scripts/activationkeys.sh
+++ scripts/activationkeys.sh
@@ -48,8 +48,33 @@ staging-sles15sp2-prod-suse-enterprise-storage-7-updates-x86_64-sp2
 staging-sles15sp2-prod-suse-packagehub-15-sp2-backports-pool-x86_64
 staging-sles15sp2-prod-suse-packagehub-15-sp2-pool-x86_64"
 
+CHILDCHANNELS="staging-sles15sp2-prod-sle-manager-tools15-pool-x86_64-sp2
+staging-sles15sp2-prod-sle-manager-tools15-updates-x86_64-sp2
+staging-sles15sp2-prod-sle-module-basesystem15-sp2-pool-x86_64
+staging-sles15sp2-prod-sle-module-basesystem15-sp2-updates-x86_64
+staging-sles15sp2-prod-sle-module-containers15-sp2-pool-x86_64
+staging-sles15sp2-prod-sle-module-containers15-sp2-updates-x86_64
+staging-sles15sp2-prod-sle-module-legacy15-sp2-pool-x86_64
+staging-sles15sp2-prod-sle-module-legacy15-sp2-updates-x86_64
+staging-sles15sp2-prod-sle-module-public-cloud15-sp2-pool-x86_64
+staging-sles15sp2-prod-sle-module-public-cloud15-sp2-updates-x86_64
+staging-sles15sp2-prod-sle-module-python2-15-sp2-pool-x86_64
+staging-sles15sp2-prod-sle-module-python2-15-sp2-updates-x86_64
+staging-sles15sp2-prod-sle-module-server-applications15-sp2-pool-x86_64
+staging-sles15sp2-prod-sle-module-server-applications15-sp2-updates-x86_64
+staging-sles15sp2-prod-sle-module-web-scripting15-sp2-pool-x86_64
+staging-sles15sp2-prod-sle-module-web-scripting15-sp2-updates-x86_64
+staging-sles15sp2-prod-sle-product-ha15-sp2-pool-x86_64
+staging-sles15sp2-prod-sle-product-ha15-sp2-updates-x86_64
+staging-sles15sp2-prod-sle-product-sles15-sp2-updates-x86_64
+staging-sles15sp2-prod-sle15-sp2-installer-updates-x86_64
+staging-sles15sp2-prod-sles15sp2-ptfs
+staging-sles15sp2-prod-suse-caasp-4.5-pool-x86_64-sp2
+staging-sles15sp2-prod-suse-caasp-4.5-updates-x86_64-sp2"
+
 function _CREATEKEYS {
-for ACTIVATIONKEY in $KEYS; do 
+for ACTIVATIONKEY in $KEYS; do
+        /usr/bin/spacecmd -y -- activationkey_delete 1-$ACTIVATIONKEY
 	/usr/bin/spacecmd -y -- activationkey_create -n $ACTIVATIONKEY -d $ACTIVATIONKEY -b $(echo $BASECHANNEL|sed "s/-prod-/-$STAGE-/g");
 	/usr/bin/spacecmd -y -- activationkey_enableconfigdeployment 1-$ACTIVATIONKEY;
         /usr/bin/spacecmd -y -- activationkey_addgroups 1-$ACTIVATIONKEY $ACTIVATIONKEY;
--- scripts/assignconfigs.sh
+++ scripts/assignconfigs.sh
@@ -2,27 +2,16 @@
 
 # this does not work because the api does not exist!
 
-# infrastrucure
+# all:
 rkeadmin
-infrastructure-file-system
-infrastructure-server-components
-enable-ip-forwarding
-rsyslog-logrotate
-nfs-server
-trusted-certificate-authorities
 patch-server
 
 # load-balancer
-rkeadmin
 load-balancer-config
-trusted-certificate-authorities
-patch-server
 
 # rke-node
-rkeadmin
 rke-cluster
 enable-ip-forwarding
 disable-swap
-rke-node-filesystem
-trusted-certificate-authorities
-patch-server
+
+# rke-test / rke-int / rpk-prod
